b6ab339f5224d688c8c0c0ddcf2cc8601f37d565
Daniel <danielazmy541@gmail.com>
2025-02-12 01:46:56 +0200

    Initial Commit

---
+++ BostaTaskTests/BostaTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  BostaTaskTests.swift
+//  BostaTaskTests
+//
+//  Created by Dodo's Mac on 12/02/2025.
+//
+
+import Testing
+@testable import BostaTask
+
+struct BostaTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
